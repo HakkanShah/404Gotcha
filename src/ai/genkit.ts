b8830b6be7d0fead68ef16f26899bf3c26e8b23a
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-08-19 20:43:37 +0530

    Delete src directory

--- src/ai/genkit.ts
+++
@@ -1,9 +0,0 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-
-export const ai = genkit({
-  plugins: [googleAI({
-    apiVersion: 'v1beta'
-  })],
-  model: 'gemini-1.5-flash-latest',
-});
